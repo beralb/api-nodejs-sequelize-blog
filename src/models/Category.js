162ba881857ddf4ef61c417514f51968754bff2c
Jean Paul Bernhardt <beralb@tutanota.com>
2022-10-26 16:36:49 -0300

    req 07 concluido

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     name: DataTypes.STRING,
   }, {
     tablename: 'categories',
